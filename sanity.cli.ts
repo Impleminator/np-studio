f11a96cc492837b880ae426af179d36c238a312f
Anders Maage-Undrum <anders@muweb.no>
2026-04-14 12:36:16 +0200

    fix: Add appid to sanity config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'k5yele4cq8pvqb9micsviexy',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
